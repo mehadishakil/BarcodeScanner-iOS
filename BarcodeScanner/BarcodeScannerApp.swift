5bc12ca50ab6270ee81dff86f2528bd2021a58ed
mehadishakil <mehadishakil469@gmail.com>
2024-02-28 22:12:10 +0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Abdul Momin on 28/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
